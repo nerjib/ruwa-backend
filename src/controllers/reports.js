de6aee3d022bf3c15075265a441acc2a8a74acf0
nerjib <kabirnajib0@gmail.com>
2020-03-05 09:21:45 +0000

    modify reports to oder by id desc

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -185,7 +185,7 @@ router.put('/:id', async (req, res) => {
   });
 //reports.id,users.first_name,users.last_name,projects.gps,reports.date,users.role, reports.id, projects.title,projects.lot, projects.lga,projects.ward, projects.community,projects.facility, contractors.company, reports.id
   router.get('/completereports/all', async (req, res) => {
-    const getAllQ = 'SELECT  projects.community,projects.facility,projects.status,projects.pstatus,reports.id,users.first_name,users.last_name,projects.gps,reports.date,users.role, reports.id, projects.title,projects.lot, projects.lga,projects.ward, projects.community,projects.facility, contractors.company, reports.id from reports left join projects on projects.id=reports.pid left join contractors on contractors.id=projects.contractor_id left join users on users.id=projects.local_id where reports.complete=$1 ';
+    const getAllQ = 'SELECT  projects.community,projects.facility,projects.status,projects.pstatus,reports.id,users.first_name,users.last_name,projects.gps,reports.date,users.role, reports.id, projects.title,projects.lot, projects.lga,projects.ward, projects.community,projects.facility, contractors.company, reports.id from reports left join projects on projects.id=reports.pid left join contractors on contractors.id=projects.contractor_id left join users on users.id=projects.local_id where reports.complete=$1 order by reports.id desc';
     try {
       // const { rows } = qr.query(getAllQ);
       const { rows } = await db.query(getAllQ, [1]);
