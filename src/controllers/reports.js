8a4306e11cc2667b2da02fce61906a9ed9ab16bb
nerjib <kabirnajib0@gmail.com>
2020-03-05 23:27:45 +0000

    retrieve weekly tables for activities

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -377,6 +377,37 @@ return res.status(400).send(error);
   
 });
 
+
+router.get('/activity/weekly/:id', async (req, res) => {
+  const getAllQ = 'SELECT * FROM weeklyreportactivities WHERE rid= $1';
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ, [req.params.id]);
+    return res.status(201).send(rows);
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return res.status(400).send({ message: 'User with that EMAIL already exist' });
+    }
+    return res.status(400).send(`${error} jsh`);
+  }
+});
+
+router.get('/activity/weekly/allactivity', async (req, res) => {
+  const getAllQ = 'SELECT * FROM weeklyreportactivities';
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ);
+    return res.status(201).send(rows);
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return res.status(400).send({ message: 'User with that EMAIL already exist' });
+    }
+    return res.status(400).send(`${error} jsh`);
+  }
+});
+
+
+
   
 module.exports = router;
 
