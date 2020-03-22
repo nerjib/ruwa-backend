9334bbd29eed2a58c701380a87e841c55b082ff0
nerjib <kabirnajib0@gmail.com>
2020-03-22 10:54:39 +0000

    get state and local id

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -207,7 +207,7 @@ return res.status(400).send(error);
   });
 
   router.get('/completeprojects/sanitations', async (req, res) => {
-    const getAllQ = 'SELECT   projects.community,projects.facility,projects.lot,projects.ward,projects.pstatus,projects.id,projects.title,projects.lga,projects.location,projects.status,contractors.company,users.first_name,users.last_name from Projects left join contractors on projects.contractor_id=contractors.id left join users on users.id=projects.state_id where projects.title=$1 order by projects.id desc';
+    const getAllQ = 'SELECT  projects.local_id,projects.state_id,projects.community,projects.facility,projects.lot,projects.ward,projects.pstatus,projects.id,projects.title,projects.lga,projects.location,projects.status,contractors.company,users.first_name,users.last_name from Projects left join contractors on projects.contractor_id=contractors.id left join users on users.id=projects.state_id where projects.title=$1 order by projects.id desc';
     try {
       // const { rows } = qr.query(getAllQ);
       const { rows } = await db.query(getAllQ,['Sanitation']);
@@ -274,6 +274,9 @@ return res.status(400).send(error);
       return res.status(400).send(`${error} jsh`);
     }
   });
+
+
+
 
 module.exports = router;
 
