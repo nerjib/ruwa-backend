d4166c57b04f0d2a39ca085451bf3d38405f276d
nerjib <kabirnajib0@gmail.com>
2020-07-14 06:01:26 +0100

    get reports by phase

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -235,6 +235,22 @@ router.put('/:id', async (req, res) => {
       return res.status(400).send(`${error} jsh`);
     }
   });
+
+  router.get('/completereports/all/:phase', async (req, res) => {
+    const getAllQ = `SELECT  reports.pid,reports.activity,reports.activityoutcome,reports.conclusion,projects.phase,projects.community,projects.facility,projects.status,projects.pstatus,reports.id,users.first_name,users.last_name,projects.gps,reports.date,users.role, reports.id, projects.title,projects.lot, projects.lga,projects.ward, projects.community,projects.facility, contractors.company, reports.id from reports left join projects on projects.id=reports.pid left join contractors on contractors.id=projects.contractor_id left join users on users.id=projects.local_id 
+    where reports.complete=$1 and projects.phase=$2 order by reports.id desc`;
+    try {
+      // const { rows } = qr.query(getAllQ);
+      const { rows } = await db.query(getAllQ, [1, req.params.phase]);
+      return res.status(201).send(rows);
+    } catch (error) {
+      if (error.routine === '_bt_check_unique') {
+        return res.status(400).send({ message: 'User with that EMAIL already exist' });
+      }
+      return res.status(400).send(`${error} jsh`);
+    }
+  });
+
   router.get('/completereports/community', async (req, res) => {
     const getAllQ = 'SELECT   projects.phase,projects.community,projects.facility,projects.lot,projects.pstatus,reports.id,users.first_name,users.last_name,projects.gps,reports.date,users.role, reports.id, projects.title,projects.lot, projects.lga,projects.ward, projects.community,projects.facility, contractors.company, reports.id from reports left join projects on projects.id=reports.pid left join contractors on contractors.id=projects.contractor_id left join users on users.id=projects.local_id where reports.complete=$1 and projects.title=$2 order by reports.id desc';
     try {
