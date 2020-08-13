b866580676136c4022163f0a5f4b3854b47181f1
nerjib <kabirnajib0@gmail.com>
2020-08-13 17:08:56 +0100

    get valuation

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -119,7 +119,6 @@ router.get('/localsupervisors/donephases1/:id', async(req, res) =>{
       return res.status(400).send(error);
     }
   });
-
 }
 router.get('/statesupervisors/donephases1/:id', async(req, res) =>{
   const project = 'SELECT * FROM projects WHERE state_id=$1  order by phase desc';
@@ -269,7 +268,7 @@ return res.status(400).send(error);
   
   
   router.get('/completeprojects/all', async (req, res) => {
-    const getAllQ = 'SELECT  projects.phase,projects.gps,projects.local_id,projects.state_id,projects.community,projects.facility,projects.lot,projects.pstatus,projects.id,projects.title,projects.lga,projects.ward,projects.status,contractors.company,users.first_name,users.last_name,users.other_name from Projects left join contractors on projects.contractor_id=contractors.id left join users on users.id=projects.state_id order by projects.lot asc, projects.id desc';
+    const getAllQ = 'SELECT  projects.valuation,projects.phase,projects.gps,projects.local_id,projects.state_id,projects.community,projects.facility,projects.lot,projects.pstatus,projects.id,projects.title,projects.lga,projects.ward,projects.status,contractors.company,users.first_name,users.last_name,users.other_name from Projects left join contractors on projects.contractor_id=contractors.id left join users on users.id=projects.state_id order by projects.lot asc, projects.id desc';
     try {
       // const { rows } = qr.query(getAllQ);
       const { rows } = await db.query(getAllQ);
