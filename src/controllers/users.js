706629ced587fd6e1a1f27eb193f996167a12fbd
nerjib <kabirnajib0@gmail.com>
2020-08-13 21:13:18 +0100

    get single monitor

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -71,6 +71,20 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+router.get('/monitors/:id', async (req, res) => {
+  const getAllQ = 'SELECT * FROM monitors WHERE id= $1';
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ, [req.params.id]);
+    return res.status(201).send(rows);
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return res.status(400).send({ message: 'User with that EMAIL already exist' });
+    }
+    return res.status(400).send(`${error} jsh`);
+  }
+});
+
 router.get('/signin/:email', async (req, res) => {
   const getAllQ = 'SELECT * FROM users WHERE email= $1';
   try {
