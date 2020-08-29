f9589b5410bb415ed99743ed07c1b1968dc04c43
nerjib <kabirnajib0@gmail.com>
2020-08-29 04:14:48 +0100

    Vip update

--- src/controllers/updates.js
+++ src/controllers/updates.js
@@ -273,4 +273,267 @@ router.get('/hpbh', async (req, res) => {
    res.status(201).json(kk12)
 })
 
+
+
+
+const getVipPid = async()=>{
+  const getAllQ = `insert into vipcov(pid) select id from projects where title=$1 and id not in (select pid from hpbhcov)`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,['Sanitation']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+
+}
+
+const getVipTos = async()=>{
+  const getAllQ = `update vipcov set tos=10 from reports where vipcov.pid=reports.pid and vipcov.pid in (select pid from reports where pstatus=$1)`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,['TOS']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+
+}
+
+const getVipEscavation = async()=>{
+  const getAllQ = `update vipcov set tos=10,escavation=15 from reports where vipcov.pid=reports.pid and vipcov.pid in (select pid from reports where pstatus=$1)`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,['Escavation']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+
+}
+const getVipSubS = async()=>{
+  const getAllQ = `update vipcov set tos=10,escavation=15,subs=20 from reports where vipcov.pid=reports.pid and vipcov.pid in (select pid from reports where pstatus=$1)`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,['SubS']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+
+}
+
+const getVipSuperS = async()=>{
+  const getAllQ = `update vipcov set tos=10,supers=20 from reports where vipcov.pid=reports.pid and vipcov.pid in (select pid from reports where pstatus=$1)`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,['SuperS']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+
+}
+
+const getVipFinishing = async()=>{
+  const getAllQ = `update vipcov set tos=10,supers=20,finishing=15 from reports where vipcov.pid=reports.pid and vipcov.pid in (select pid from reports where pstatus=$1)`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,['Finishing']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+
+}
+
+
+const getVipArt = async()=>{
+  const getAllQ = `update vipcov set tos=10,supers=20,finishing=15,artwork=10 from reports where vipcov.pid=reports.pid and vipcov.pid in (select pid from reports where pstatus=$1)`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,['Painting']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+
+}
+
+const getVipSignBoard = async()=>{
+  const getAllQ = `update vipcov set tos=10,cr=5 from reports where vipcov.pid=reports.pid and vipcov.pid in (select pid from reports where pstatus=$1)`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,['CR']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+
+}
+
+const getVipFR = async()=>{
+  const getAllQ = `update vipcov set tos=10, fr=5 from reports where vipcov.pid=reports.pid and vipcov.pid in (select pid from reports where pstatus=$1)`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,['TOS']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+
+}
+
+
+const sumVIP = async()=>{
+  const getAllQ = `update vipcov set total=(tos+escavation+subs+supers+finishinig+artwork+cr+fr), total2=(tos+escavation+subs+supers+finishinig+artwork+cr+fr)`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }
+
+}
+
+
+
+const getVIPFr = async()=>{
+  const getAllQ = `update vipcov set fr=5,total2=100 from reports where vipcov.pid=reports.pid and hpbhcov.pid in (select pid from reports where pstatus=$1)`
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,['FR']);
+   
+    return rows;
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return ({ message: 'User with that EMAIL already exist' });
+    }
+    return (`${error} jsh`);
+
+  }  
+}
+
+
+const sumVipstages=async()=>{
+const getAllQ = `update vipcov set total=(tos+escavation+subs+supers+finishinig+artwork+cr+fr) where pid is not null`
+try {
+  // const { rows } = qr.query(getAllQ);
+  const { rows } = await db.query(getAllQ);
+ 
+  return rows;
+} catch (error) {
+  if (error.routine === '_bt_check_unique') {
+    return ({ message: 'User with that EMAIL already exist' });
+  }
+  return (`${error} jsh`);
+
+}
+
+}
+
+
+const getUpdatedVipFR = async()=>{
+const getAllQ = `update vipcov set fr=5 where pid=(select id from projects where projects.pstatus=$1 and projects.id=hpbhcov.pid)`
+try {
+  // const { rows } = qr.query(getAllQ);
+  const { rows } = await db.query(getAllQ,['FR']);
+ 
+  return rows;
+} catch (error) {
+  if (error.routine === '_bt_check_unique') {
+    return ({ message: 'User with that EMAIL already exist' });
+  }
+  return (`${error} jsh`);
+
+}  
+}
+
+const getFRVip = async()=>{
+const getAllQ = `update vipcov set total2=100 where fr=$1`
+try {
+  // const { rows } = qr.query(getAllQ);
+  const { rows } = await db.query(getAllQ,[5]);
+ 
+  return rows;
+} catch (error) {
+  if (error.routine === '_bt_check_unique') {
+    return ({ message: 'User with that EMAIL already exist' });
+  }
+  return (`${error} jsh`);
+
+}  
+}
+
+
+
+router.get('/vip', async (req, res) => {
+    
+  let kk= await getVipPid() 
+    let kk1= await getVipTos()
+    let kk2= await getVipEscavation() 
+    let kk3= await getVipSubS() 
+    let kk5= await getVipSuperS() 
+    let kk6= await getVipFinishing()
+    let kk10= await getVipArt()
+    let kk7= await getVipSignBoard()
+    let kk9 = await getFRVip()
+    let kk8= await getVIPFr()
+    let kk11= await getUpdatedVipFR()
+    let kk4= await sumVIP()
+
+   res.status(201).json(kk11)
+})
+
 module.exports = router;
