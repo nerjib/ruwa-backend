9f2cc59718f4026186ba23e47cf575f025b9721f
nerjib <kabirnajib0@gmail.com>
2020-08-31 09:33:49 +0100

    get updated progress

--- src/controllers/updates.js
+++ src/controllers/updates.js
@@ -827,6 +827,20 @@ router.get('/solar', async (req, res) => {
 })
 
 
+router.get('/allsol', async (req, res) => {
+  const getAllQ = 'SELECT * from solarcov';
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,[req.params.phase]);
+    return res.status(201).send(rows);
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return res.status(400).send({ message: 'User with that EMAIL already exist' });
+    }
+    return res.status(400).send(`${error} jsh`);
+  }
+});
+
 router.get('/allsolar', async (req, res) => {
   const getAllQ = `select * from solarcov`
   try {
