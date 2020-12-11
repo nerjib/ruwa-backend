5a4d201a3d6e972fb73e5abcca04382c908acace
nerjib <kabirnajib0@gmail.com>
2020-12-11 10:56:28 +0100

    posting and getting site taking over

--- src/controllers/monitorsReport.js
+++ src/controllers/monitorsReport.js
@@ -205,7 +205,7 @@ router.post('/', async (req, res) => {
   router.post('/toshos', async (req, res) => {
     const createUser = `INSERT INTO
     siteover(pid, sid,gps,existence,activity,info, imgurl, gentime, time)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8,$9) RETURNING *`;
   //console.log(req.body)
   const values = [
   req.body.pid,
