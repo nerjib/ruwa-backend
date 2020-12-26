fc376406876a8ac580f32fd6258497fbaf554499
nerjib <kabirnajib0@gmail.com>
2020-12-26 18:04:48 +0100

    search users by phone no

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -84,6 +84,20 @@ router.get('/email/:email', async (req, res) => {
     return res.status(400).send(`${error} jsh`);
   }
 });
+router.get('/searchuser/:id', async (req, res) => {
+  const getAllQ = 'SELECT * FROM   users  where phone like $1';
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,['%'+req.params.id+'%']);
+    return res.status(201).send(rows);
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return res.status(400).send({ message: 'User with that EMAIL already exist' });
+    }
+    return res.status(400).send(`${error} jsh`);
+  }
+});
+
 
 router.get('/monitors/:id', async (req, res) => {
   const getAllQ = 'SELECT * FROM monitors WHERE id= $1';
