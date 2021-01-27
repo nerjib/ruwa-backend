241b8de7062599b6c1ea5427c3e1b0b9271f134b
nerjib <kabirnajib0@gmail.com>
2021-01-27 10:57:54 +0100

    updated functionality status

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -376,18 +376,16 @@ const updateprojectfunc = async(e,pid)=>{
 }
 
 router.put('/functionalitystatus', async (req, res) => {
+
   /*if(req.body.status=='accepted'){
    await updateprojectfunc(req.body.functionality,req.body.pid)
-
   }*/
  // console.log('kkkk')
-const updateReport = `UPDATE
-followupreports SET status=$1 WHERE id=$2
- RETURNING *`;
+const updateReport = `UPDATE followupreports SET status=$1 WHERE id=$2 RETURNING *`;
 console.log(req.body)
 const values = [
-req.body.status,
-req.body.id
+'req.body.status',
+142
 ];
 try {
 const { rows } = await db.query(updateReport, values);
